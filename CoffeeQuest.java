1e3bd3656a10cf5db6203c80703b3273a9b04bc9
Colin <colin.swan@pitt.edu>
2016-02-04 19:54:10 -0500

    Initial commit

---
+++ CoffeeQuest.java
@@ -0,0 +1,18 @@
+/*
+ * Program: Coffee Quest!
+ * Description: Another day, another quest for coffee. Embark on an epic
+ * journey - to a magical land - filled - with coffee. Join Bob, the hero
+ * of our tale, as he struggles to find the one thing he could never possibly
+ * live without. Coffee, with sugar. And. Cream. Will he survive? Bob's fate
+ * rests in your incapable hands.
+ *
+ * Author: Colin Swan
+ * Due date: February 15, 2016
+ */
+
+public class CoffeeQuest{
+	public static void main(String[] args){
+
+	}
+}
+
