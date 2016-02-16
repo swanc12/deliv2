ef7648463a3b91d38fa291020410b29bcd7bf8dd
Colin Swan <colin.swan@pitt.edu>
2016-02-16 02:09:38 -0500

    Possibly final commit. Added TestSuite to coalesce tests into one suite, allowing code coverage to work properly. Added docs folder which has the writeup as well as pictures for test cases and coverage.

---
+++ TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.runners.Suite;
+import org.junit.runner.RunWith;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	CoffeeQuestTest.class,
+	PlayerTest.class,
+	CoffeeQuestOutputTest.class,
+	RoomTest.class
+})
+public class TestSuite{
+	
+}
